0e50acf6c44c7ee50cea77fa07e502a9a3c0d07c
mArker <252133226@qq.com>
2017-03-10 15:53:50 +0800

    add rules

--- index.js
+++ index.js
@@ -1,11 +1,70 @@
 module.exports = {
+  extends: ['eslint-config-airbnb'].map(require.resolve),
   env: {
+    browser: true,
+    jasmine: true,
+    mocha: true,
     node: true,
-    browser: true
+    jest: true,
+    es6: true
   },
-  globals: {},
-  extends: ['airbnb'].map(require.resolve),
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true
+    }
+  },
+  plugins: [
+    'eslint-plugin-markdown',
+    'eslint-plugin-import',
+    'eslint-plugin-react',
+    'eslint-plugin-jsx-a11y'
+  ].map(require.resolve),
   rules: {
-    semi: [2, 'always']
+    // use double quotes for copy friendly
+    "func-names": [0],
+    "semi": [2, "always"],
+    "comma-dangle": [2, "never"],
+    "space-in-parens": [2, "always", {
+      "exceptions": ["{}", "[]", "()", "empty"]
+    }],
+    "object-curly-spacing": [2, "always", {
+      "arraysInObjects": false,
+      "objectsInObjects": false
+    }],
+    "prefer-arrow-callback": [2],
+    "arrow-parens": [2, "as-needed"],
+    "arrow-body-style": [2, "as-needed"],
+    "space-before-function-paren": [2, {
+      "named": "never",
+      "anonymous": "always",
+      "asyncArrow": "ignore"
+    }],
+    "consistent-return": [2, {
+      "treatUndefinedAsUnspecified": false
+    }],
+    "no-plusplus": [2, {
+      "allowForLoopAfterthoughts": true
+    }],
+    "no-unused-vars": [2, {
+      "args": "all",
+      "caughtErrors": "none",
+      "argsIgnorePattern": "^_|_$"
+    }],
+    "no-underscore-dangle": [0],
+    "padded-blocks": [0],
+    "import/newline-after-import": [0],
+    "no-mixed-operators": [2, {
+      "allowSamePrecedence": true
+    }],
+    "react/jsx-filename-extension": [1, {
+      "extensions": [".js", ".jsx"]
+    }],
+    "import/extensions": [0],
+    "react/prop-types": [0],
+    "react/prefer-stateless-function": [0],
+    "no-nested-ternary": [0],
+    "no-bitwise": [0],
+    "require-yield": [1],
+    "jsx-a11y/no-static-element-interactions": [0]
   }
-}
+};
